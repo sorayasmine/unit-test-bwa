20ddbba516d6b4cfb7966b25af1b81d3d1d33630
tiara <niceefforts42@gmail.com>
2022-01-05 14:47:39 +0700

    rearrange files

---
+++ basic_vue_test_utils/src/Greeting.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <p ref="labelInputRef">Input your name</p>
+    <input ref="inputRef" type="text" v-model="name" />
+    <button ref="buttonRef" @click="isBtnClicked = true">click</button>
+    <p ref="greetingRef" v-if="isBtnClicked">Welcome, {{ name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  // components: {
+  //   HelloWorld,
+  data() {
+    return {
+      name: "",
+      isBtnClicked: false,
+    };
+  },
+  methods: {
+    inputName: function (name) {
+      console.log(this.name);
+      return `Welcome ${name}`;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
